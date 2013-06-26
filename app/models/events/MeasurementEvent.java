3735dfdb6a4d48be1bd80a66a3f4ed1ed5b41645
JP Wesselink <jp.wesselink@wearereasonablepeople.com>
2013-06-26 23:01:06 +0200

    One can not simply hand over JPA entities

--- app/models/events/MeasurementEvent.java
+++ app/models/events/MeasurementEvent.java
@@ -1,18 +1,38 @@
 package models.events;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import models.Measurement;
+import javax.persistence.Column;
+
+import play.data.validation.Required;
 
 public class MeasurementEvent extends Event {
 	final public Map<Integer, Float> percentages;
 	final public Measurement measurement;
+	
+	// HACK: not enough time
+	public static class Measurement {
+		public Date createdAt = new Date();
+		
+		public Integer value;
+		
+		public double lat;
+		
+		public double lng;
+		
+		public Measurement(models.Measurement measurement){
+			createdAt = measurement.createdAt;
+			value = measurement.value;
+			lat = measurement.lat;
+			lng = measurement.lng;
+		}
+	}
 	public MeasurementEvent(String type) {
 		super("measurement");
 		percentages = new HashMap<Integer, Float>();
 		measurement = null;
-		// TODO Auto-generated constructor stub
 	}
 
 	public MeasurementEvent(Map<Integer, Float> percentages) {
@@ -20,9 +40,9 @@ public class MeasurementEvent extends Event {
 		this.percentages = percentages;
 		measurement = null;
 	}
-	public MeasurementEvent(Measurement measurement, Map<Integer, Float> percentages) {
+	public MeasurementEvent(models.Measurement measurement, Map<Integer, Float> percentages) {
 		super("measurement");
-		this.measurement = measurement;
+		this.measurement = new Measurement(measurement);
 		this.percentages = percentages;
 	}
 
